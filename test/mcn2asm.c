57d4dc8ee03fcfe604cb8fa08d9a00acf10aad7c
TheHaruking <wta2m-c3@yahoo.co.jp>
2016-12-02 21:40:30 +0900

    Changes to be committed: 	renamed:    mcn2asm.c -> test/mcn2asm.c
     Changes not staged for commit:
    	modified:   source/mcn2asm.c
     modified:   source/mcn2asm.h
    	deleted:    test/mcn2asm.c

--- test/mcn2asm.c
+++ test/mcn2asm.c
@@ -1,30 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "mcn2asm.h"
 
-struct asmarray {
-	unsigned char head;
-	unsigned char _unused;
-	unsigned short code[7];
-};
-
-const char head_tbl[32] = {
+static const char head_tbl[32] = {
+//  0  1  2  3  4  5  6  7
 	5, 5, 5, 6, 4, 4, 4, 4,
-	1, 1, 1, 1, 1, 1, 1, 1,
-	1, 1, 1, 1, 1, 1, 1, 1,
-	1, 1, 1, 1, 1, 1, 1, 1,
+	4, 3, 7, 7, 6, 6, 6, 6,
+	5, 5, 4, 4, 4, 4, 5, 5,
+	4, 4, 3, 3, 2, 1, 3, 3,
 };
+
+static void sethead(struct asmarray* asmcode, unsigned short mcncode){
+	asmcode->head = head_tbl[mcncode >> 11];
+}
+
+static void setcode(struct asmarray* asmcode, unsigned short mcncode){
+	int p = asmcode->head - 1;
+	asmcode->code[p] = mcncode & 0x7;
+	p--;	mcncode >>= 3;
+	asmcode->code[p] = mcncode & 0x7;
+	p--;	mcncode >>= 3;
+}
 
 void McnToAsm(struct asmarray* asmcode, unsigned short mcncode){
-	asmcode->head = head_tbl[mcncode >> 11];
-	
+	sethead(asmcode, mcncode);	
+	setcode(asmcode, mcncode);
 }
 
-void TestMcnToAsm(){
-
-}
-
-void PrintMcnAndAsm(unsigned short mcncode, struct asmarray asmcode){
+static void PrintMcnAndAsm(unsigned short mcncode, struct asmarray asmcode){
 	printf("mcn = %04X\n", mcncode);
 	printf("asm = ");
 	for(int i = 0; i < asmcode.head; i++){
@@ -37,9 +41,10 @@ int main(){
 	unsigned short mcncode;
 	struct asmarray asmcode;
 
-	mcncode = 0x1234;
+	mcncode = 0x5004;
 	asmcode.head = 0;
 	memset(asmcode.code, 0, 7);
+
 	PrintMcnAndAsm(mcncode, asmcode);
 	McnToAsm(&asmcode, mcncode);
 	PrintMcnAndAsm(mcncode, asmcode);
