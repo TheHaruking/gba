e4327b001712900955426b0197ca1c391b25fa54
TheHaruking <wta2m-c3@yahoo.co.jp>
2016-12-02 21:45:33 +0900

    Changes to be committed: 	new file:   test/.testmcn2asm.c.swp 	new file:   test/mkmcn2asm.sh 	new file:   test/testmcn2asm.c 	new file:   test/testmcn2asm.out

---
+++ test/mkmcn2asm.sh
@@ -0,0 +1,1 @@
+gcc testmcn2asm.c ../source/mcn2asm.c -o testmcn2asm.out
---
+++ test/testmcn2asm.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "../source/mcn2asm.h"
+
+static void PrintMcnAndAsm(unsigned short mcncode, struct asmarray asmcode){
+	printf("mcn = %04X\n", mcncode);
+	printf("asm = ");
+	for(int i = 0; i < asmcode.head; i++){
+		printf("%d ", asmcode.code[i]);
+	} printf("\n");
+}
+
+
+int main(){
+	unsigned short mcncode;
+	struct asmarray asmcode;
+
+	printf("mcncode? : ");
+	scanf("%4hx", &mcncode) ;
+	asmcode.head = 0;
+	memset(asmcode.code, 0, 7);
+
+	PrintMcnAndAsm(mcncode, asmcode);
+	McnToAsm(&asmcode, mcncode);
+	PrintMcnAndAsm(mcncode, asmcode);
+	return 0;
+}
